bb6f760634b1d165846789a1e68094396fa426e6
jhj9109 <jhj91_09@naver.com>
2022-05-25 21:05:27 +0900

    Chorus | edit header comment |

--- 01_sort/sort.h
+++ 01_sort/sort.h
@@ -8,10 +8,16 @@ typedef struct e_node {
 	struct e_node	*r_node;
 }				t_node;
 
+/* 1.sort_by_conditions */
 void	sort_2el(int *arr, int *newArr);
 void	sort_3el(int *arr, int *newArr);
+
+/* 2.bubble_sort */
 void	bubble_sort(int *arr, int len, int *cnt);
+
+/* 3.insert_sort */
 void	insert_sort(int *arr, int len, int *newArr, int *cnt);
+
 void	print_array(char *s, int *a, int end, int cnt);
 
 /* 4.heap_sort */
