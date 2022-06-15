5900c272836cbcd19336b7791f97377130ccff0a
jhj9109 <jhj91_09@naver.com>
2022-06-15 21:13:33 +0900

    Add | recurrence_dp_top_bottom |

--- 03_recurrence_relation/recurrence.h
+++ 03_recurrence_relation/recurrence.h
@@ -9,10 +9,15 @@ typedef	struct s_germ
 
 /* 1.recurrence_iter */
 int	recurrence_iter(int n, int k);
+
 /* 2.recurrence_dp_bottom_top */
 int	recurrence_dp_bottom_top(int n, int k);
-/* 2.recurrence_recursive */
+
+/* 3.recurrence_recursive */
 int	recurrence_recursive(int n, int k);
+
+/* 4.recurrence_dp_top_bottom */
+int	recurrence_dp_top_bottom(int n, int k);
 /* recurrence_utils */
 t_germ	*ft_obj_new(int delay);
 t_germ	*ft_obj_last(t_germ *head);
---
+++ 03_recurrence_relation/recurrence_dp_top_bottom.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+static int	_re_with_dp(int *arr, int n, int k)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 1)
+		return (0);
+	else if (arr[n])
+		return (arr[n]);
+	arr[n] = _re_with_dp(arr, n - 1, k) + _re_with_dp(arr, n - k, k);
+	return (arr[n]); 
+}
+
+int	recurrence_dp_top_bottom(int n, int k)
+{
+	int *arr;
+	int n;
+	int	k;
+	int	result;
+
+	arr = calloc(n + 1, sizeof(int));
+	if (!arr)
+		exit(EXIT_FAILURE);
+	result = _re_with_dp(arr, n, k);
+	free(arr);
+	return (result);
+}
