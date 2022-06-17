0b9cb012fd7bc3717c9564e2f0265d7d9a4fe9cc
jhj9109 <jhj91_09@naver.com>
2022-06-17 23:18:27 +0900

    Update add func |recurrence_matrix() & ft_atoll()|

--- 03_recurrence_relation/recurrence.h
+++ 03_recurrence_relation/recurrence.h
@@ -7,6 +7,12 @@ typedef	struct s_germ
 	struct s_germ *next;
 }	t_germ;
 
+typedef enum s_stat
+{
+	ERROR = -1,
+	SUCCESS
+}	t_stat;
+
 /* 1.recurrence_iter */
 int	recurrence_iter(int n, int k);
 
@@ -18,6 +24,10 @@ int	recurrence_recursive(int n, int k);
 
 /* 4.recurrence_dp_top_bottom */
 int	recurrence_dp_top_bottom(int n, int k);
+
+/* 5.recurrence_matrix */
+int	recurrence_matrix(int n, int k);
+
 /* recurrence_utils */
 t_germ	*ft_obj_new(int delay);
 t_germ	*ft_obj_last(t_germ *head);
@@ -25,5 +35,6 @@ void	ft_obj_pushback(t_germ **head, t_germ *new_node);
 int		ft_obj_size(t_germ *head);
 void	ft_obj_clear(t_germ **head);
 void	arr_print(int *arr, int end);
+int		ft_atoll(const char *str, long long *n);
 
 #endif
